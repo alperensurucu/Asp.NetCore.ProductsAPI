ca3ab9f4ea26b71b2fba08dce8bd09f81b9abe36
alper <alperensurucu.eng@gmail.com>
2024-04-17 22:53:58 +0300

    added.HttpStatusCodes

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Asp.NetCore.ProductsAPI.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Asp.NetCore.ProductsAPI.Controllers
 {
@@ -8,23 +9,45 @@ namespace Asp.NetCore.ProductsAPI.Controllers
 
     public class ProductsController : ControllerBase
     {
-        public static readonly string[] Products = {
-        
-            "IPhone 14","IPhone 15","IPhone 16"
-        };
+        private static List<Product>? _products;
+
         //localhost:5000/api/prodcuts =>GET
 
-        [HttpGet]
-        public string[] GetProducts()
+        public ProductsController()  
         {
-            return Products; //Products listesini geri gönderir.
+            _products = new List<Product>
+            {
+                new () { ProductId = 1, ProductName = "IPhone14", Price = 60000, IsActive = true },
+                new () { ProductId = 2, ProductName = "IPhone15", Price = 70000, IsActive = true },
+                new () { ProductId = 3, ProductName = "IPhone16", Price = 80000, IsActive = true },
+                new () { ProductId = 4, ProductName = "IPhone17", Price = 90000, IsActive = true }
+            };
+        }    
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            if (_products == null)
+            {
+                return NotFound();
+            }
+            return Ok(_products);
         }
 
         //localhost:5000/api/prodcuts/1 =>GET
+
         [HttpGet("{id}")]
-        public string GetProduct(int id)
+        public IActionResult GetProduct(int? id)
         {
-            return Products[id]; //Products listesini geri gönderir. İd filtrelemesiyle ama .
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var p  =_products?.FirstOrDefault(i => i.ProductId == id);
+            if (p == null)
+            {
+                return NotFound();
+            }
+            return Ok(p);
         }
     }
     
---
+++ Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace Asp.NetCore.ProductsAPI.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } = null!; 
+        public decimal Price { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
